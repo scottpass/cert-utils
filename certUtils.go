c9ef5cf780146012b3ac662de90e9378cdca0223
Scott Wisniewski <scott@scottdw2.com>
2023-10-30 20:55:03 -0700

    Add methods to read / write a "encryption target key" extension to / from scottpass device certs.

--- certUtils.go
+++ certUtils.go
@@ -2,13 +2,17 @@ package cert_utils
 
 import (
 	"crypto/ecdsa"
+	"crypto/elliptic"
 	"crypto/sha256"
 	"crypto/x509"
+	"crypto/x509/pkix"
 	"encoding/hex"
 	"encoding/pem"
 	"errors"
 	"fmt"
 )
+
+var EncryptionTargetKeyExtensionOID = []int{1, 3, 9942, 1, 1}
 
 func CertToPem(cert *x509.Certificate) string {
 	return string(pem.EncodeToMemory(&pem.Block{
@@ -72,4 +76,41 @@ func PemFromPubKey(pub *ecdsa.PublicKey) (string, error) {
 		Type:  "PUBLIC KEY",
 		Bytes: der,
 	})), nil
+}
+
+func GetEncryptionTargetKey(cert *x509.Certificate) (*ecdsa.PublicKey, error) {
+	for _, ext := range cert.Extensions {
+		if ext.Id.Equal(EncryptionTargetKeyExtensionOID) {
+			pubAny, err := x509.ParsePKIXPublicKey(ext.Value)
+			if err != nil {
+				return nil, err
+			}
+			ret, ok := pubAny.(*ecdsa.PublicKey)
+			if !ok {
+				return nil, errors.New("encryption target key is not an ECC key")
+			}
+			if ret.Curve != elliptic.P256() {
+				return nil, errors.New("encryption target key does not use the P256 curve")
+			}
+			return ret, nil
+		}
+	}
+	return nil, errors.New("cert is missing the encryption target key extension")
+}
+
+func AddEncryptionTargetKey(cert *x509.Certificate, key *ecdsa.PublicKey) error {
+	derBytes, err := x509.MarshalPKIXPublicKey(key)
+	if err != nil {
+		return err
+	}
+
+	cert.ExtraExtensions = append(
+		cert.ExtraExtensions,
+		pkix.Extension{
+			Id:       EncryptionTargetKeyExtensionOID,
+			Critical: false,
+			Value:    derBytes,
+		},
+	)
+	return nil
 }
